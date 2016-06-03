e0e9cec10a84628d95fe2487601c386746145847
Richard Buchanan <richard_buchanan@buchanandesigngroup.com>
2016-06-03 12:37:09 -0400

    Moved dropdown component to library.

--- theme/links--system-main-menu.tpl.php
+++ theme/links--system-main-menu.tpl.php
@@ -8,6 +8,7 @@
 $menu_name = variable_get('menu_main_links_source', 'main-menu');
 $menu_tree = menu_tree($menu_name);
 $menu_tree['#theme_wrappers'] = array('menu_tree__system_main');
+$library_path = _uikit_get_library_path();
 
 // Create custom theme wrapper to theme the offcanvas menu.
 if (in_array('uk-nav-offcanvas', $variables['attributes']['class'])) {
@@ -16,14 +17,15 @@ if (in_array('uk-nav-offcanvas', $variables['attributes']['class'])) {
 
 $dropdown_support = theme_get_setting('main_menu_dropdown_support');
 
+if ($dropdown_support) {
+  drupal_add_js($library_path . '/js/core/dropdown.js', array(
+    'type' => 'file',
+    'group' => JS_THEME,
+  ));
+}
+
 foreach ($menu_tree as $key => $value) {
   if (isset($value['#below']) && !empty($value['#below']) && $dropdown_support) {
-    $theme = drupal_get_path('theme', 'uikit');
-    drupal_add_js($theme . '/js/core/dropdown.js', array(
-      'type' => 'file',
-      'group' => JS_THEME,
-    ));
-
     // Add required attributes for dropdown support.
     $menu_tree[$key]['#attributes']['class'][] = 'uk-parent';
     $menu_tree[$key]['#attributes']['data-uk-dropdown'] = '';
--- theme/links--system-secondary-menu.tpl.php
+++ theme/links--system-secondary-menu.tpl.php
@@ -8,6 +8,7 @@
 $menu_name = variable_get('menu_secondary_links_source', 'user-menu');
 $menu_tree = menu_tree($menu_name);
 $menu_tree['#theme_wrappers'] = array('menu_tree__system_secondary');
+$library_path = _uikit_get_library_path();
 
 // Create custom theme wrapper to theme the offcanvas menu.
 if (in_array('uk-nav-offcanvas', $variables['attributes']['class'])) {
@@ -16,13 +17,15 @@ if (in_array('uk-nav-offcanvas', $variables['attributes']['class'])) {
 
 $dropdown_support = theme_get_setting('secondary_menu_dropdown_support');
 
+if ($dropdown_support) {
+  drupal_add_js($library_path . '/js/core/dropdown.js', array(
+    'type' => 'file',
+    'group' => JS_THEME,
+  ));
+}
+
 foreach ($menu_tree as $key => $value) {
   if (isset($value['#below']) && !empty($value['#below']) && $dropdown_support) {
-    $theme = drupal_get_path('theme', 'uikit');
-    drupal_add_js($theme . '/js/core/dropdown.js', array(
-      'type' => 'file',
-      'group' => JS_THEME,
-    ));
     $menu_tree[$key]['#attributes']['class'][] = 'uk-parent';
     $menu_tree[$key]['#attributes']['data-uk-dropdown'] = '';
     $menu_tree[$key]['#below']['#theme_wrappers'] = array('menu_tree__system_secondary__sub_menu');
