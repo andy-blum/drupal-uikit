5b282bb0ca82541344461149112d46015c519485
Richard Buchanan <richard_buchanan@buchanandesigngroup.com>
2017-01-29 23:02:57 -0500

    Removed javascript files which override core javascript files.

--- uikit_core/js/overrides.js
+++ uikit_core/js/overrides.js
@@ -1,16 +1,19 @@
 /**
  * @file
+ * Attaches behaviors to the UIkit theme.
  */
 
 (function ($) {
-  $('a').each(function() {
+  'use strict';
+
+  $('a').each(function () {
     var href = $(this).attr('href');
 
     if (href === '# ') {
       $(this).attr('href', '#');
 
       if ($(this).parent('[data-uk-dropdown]').length) {
-        $(this).attr('href', '').on('click', function(e) {
+        $(this).attr('href', '').on('click', function (e) {
           e.preventDefault();
         });
       }
