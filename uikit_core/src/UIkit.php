b7e7c47428ca27f29b8e899859208e6403ac48b5
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-04-17 06:19:31 -0400

    Various improvements to theme settings and replacement of PHP constants with UIkit class definition constants

---
+++ uikit_core/src/UIkit.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\uikit;
+
+/**
+ * Provides helper functions for the UIkit base theme.
+ */
+class UIkit {
+
+  /**
+   * The UIkit library project page.
+   *
+   * @var string
+   */
+  const UIKIT_LIBRARY = 'https://getuikit.com/';
+
+  /**
+   * The UIkit library version supported in the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_LIBRARY_VERSION = '3.0.0-beta.21';
+
+  /**
+   * The Drupal project page for the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_PROJECT = 'https://www.drupal.org/project/uikit';
+
+  /**
+   * The Drupal project branch for the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_PROJECT_BRANCH = '7.x-3.x';
+
+  /**
+   * The Drupal project API site for the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_PROJECT_API = 'http://uikit-drupal.com/api/uikit/7.x-3.x';
+
+  /**
+   * The jQuery library version UIkit supports.
+   *
+   * @var string
+   */
+  const UIKIT_JQUERY_VERSION = '2.1.4';
+
+  /**
+   * The jQuery Migrate library version UIkit supports.
+   *
+   * @var string
+   */
+  const UIKIT_JQUERY_MIGRATE_VERSION = '1.4.1';
+}
