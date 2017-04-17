257b25885a6c867d69888e22726fef84a93a352d
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-04-17 03:54:52 -0400

    Update theme settings and add UIkit class definition

---
+++ uikit_core/src/UIkit.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uikit\UIkit.
+ */
+
+namespace Drupal\uikit;
+
+/**
+ * Provides helper functions for the UIkit base theme.
+ */
+class UIkit {
+
+  /**
+   * The UIkit library project page.
+   *
+   * @var string
+   */
+  const UIKIT_LIBRARY = 'https://getuikit.com/v2/';
+
+  /**
+   * The UIkit library version supported in the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_LIBRARY_VERSION = '2.27.2';
+
+  /**
+   * The Drupal project page for the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_PROJECT = 'https://www.drupal.org/project/uikit';
+
+  /**
+   * The Drupal project branch for the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_PROJECT_BRANCH = '7.x-2.x';
+
+  /**
+   * The Drupal project API site for the UIkit base theme.
+   *
+   * @var string
+   */
+  const UIKIT_PROJECT_API = 'http://uikit-drupal.com';
+}
