3bd21ba069391fcb7a90465ae19aacb447ceec51
Andy Blum <ablum@midnetmedia.com>
2018-10-30 10:27:24 -0400

    Updated UIkit library to version 3.0.0-rc20

--- src/UIkit.php
+++ src/UIkit.php
@@ -19,7 +19,7 @@ class UIkit {
    *
    * @var string
    */
-  const UIKIT_LIBRARY_VERSION = '3.0.0-rc.17';
+  const UIKIT_LIBRARY_VERSION = '3.0.0-rc.20';
 
   /**
    * The Drupal project page for the UIkit base theme.
