76501052383674933e3e90ab4fdc55cc6b93a774
Andy Blum <andy.blum.01@gmail.com>
2019-06-26 09:07:52 -0400

    Revert UIKit libraries to 3.1.5

--- src/UIkit.php
+++ src/UIkit.php
@@ -19,7 +19,7 @@ class UIkit {
    *
    * @var string
    */
-  const UIKIT_LIBRARY_VERSION = '3.1.6';
+  const UIKIT_LIBRARY_VERSION = '3.1.5';
 
   /**
    * The Drupal project page for the UIkit base theme.
